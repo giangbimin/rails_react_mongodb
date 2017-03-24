a65524deb34edcff37106bb8c8ce0cec12b9c876
gianghoangdev <giang.nh@neo-lab.vn>
2017-03-24 13:57:16 +0700

    add name and rememberme in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@ class User
   field :current_sign_in_ip, type: String
   field :last_sign_in_ip,    type: String
 
+  field :name
+  validates :name, presence: true
+  validates :name, :email, uniqueness: { case_sensitive: false }
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
   ## Confirmable
   # field :confirmation_token,   type: String
   # field :confirmed_at,         type: Time
